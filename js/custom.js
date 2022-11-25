13f9f3255d5b781f9d3f730e6705b76a0a0aeec2
Laura Valiente <lauravcruzdaw@gmail.com>
2022-11-25 11:19:10 +0100

    formacion completad

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,30 @@
+let nav = document.getElementById("open");
+/*NAV*/
 function desplegar() {
-  let nav = document.getElementById("open");
   if (nav.classList.contains("h-100")) {
     nav.classList.remove("h-100");
   } else {
     nav.classList.add("h-100");
   }
 }
+
+
+/*ANIMATION SCROLL DOWN*/
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 95;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+reveal();
